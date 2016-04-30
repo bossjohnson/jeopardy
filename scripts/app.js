6bacbd590d42645f6aef751943d592a5015ce7b7
Josh Newsom <newsommeister@gmail.com>
2016-04-30 13:18:55 -0600

    Categories populate on load

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 $(function() {
-    $.ajax({
+    var categories = $.ajax({
         method: 'GET',
         dataType: 'json',
         url: 'http://jservice.io/api/random?count=6'
@@ -7,20 +7,57 @@ $(function() {
 
 }); //End of wrapper function
 
+
+
+
+
+
+
 // =======
 // Helpers
 // =======
-// function logClues(clues) {
-//   for (var clue of clues) {
-//     console.log(clue.category.title.toUpperCase(), clue.category.id);
-//   }
-// }
-
+var categoriesThisRound = {};
 
 function setCategories(data) {
+
     for (var i = 0; i < data.length; i++) {
-        var category = data[i].category.title.toUpperCase();
-        $('.column:nth-child(' + (i + 1) + ') .category').text(category);
-        console.log(data[i]);
+        categoriesThisRound[data[i].category.title] = data[i].category.id;
+        $('.column:nth-child(' + (i + 1) + ') .category').text(data[i].category.title.toUpperCase());
+    }
+
+    var categoryIDs = [];
+    for (var category of data) {
+        var clue = $.ajax({
+            method: 'GET',
+            dataType: 'json',
+            url: 'http://jservice.io/api/clues?category=' + category.category_id
+        });
+        categoryIDs.push(clue);
+    }
+    Promise.all(categoryIDs).then(setQuestionValues);
+}
+
+function setQuestionValues(categories) {
+
+    for (var category of categories) {
+        var questions = [];
+        for (var question of category) {
+            // console.log("Question: ", question.question);
+            questions.push(question);
+        }
+        questions.sort(function(a, b) {
+            return a.value - b.value;
+        });
+        // console.log(questions, questions.length);
+
+        // for (var i = 1; i < 7; i++) {
+        //   var $column = $('.column:nth-of-type(' + i +')');
+        //   console.log("i:", i);
+        //   for (var j = 1; j < 6; j++) {
+        //     console.log("j:", j);
+        //   }
+
+            // console.log(questions[i]);
+        // }
     }
 }
