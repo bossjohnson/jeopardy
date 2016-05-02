fab5c6b38377607b4983d7d950ec0aa5cf28075f
Josh Newsom <newsommeister@gmail.com>
2016-05-02 10:51:36 -0600

    Clicking a clue value logs the question

--- scripts/app.js
+++ scripts/app.js
@@ -1,63 +1,78 @@
 $(function() {
-    var categories = $.ajax({
-        method: 'GET',
-        dataType: 'json',
-        url: 'http://jservice.io/api/random?count=6'
-    }).done(setCategories);
+    currentRound = 1;
+    var searchCategories = $.ajax({
+            method: 'GET',
+            dataType: 'json',
+            url: 'http://jservice.io/api/random?count=6'
+        }).done(populateCategories)
+        .then(getQuestions)
+        .then(attachHandlers);
 
 }); //End of wrapper function
-
-
-
-
 
 
 
 // =======
 // Helpers
 // =======
-var categoriesThisRound = {};
 
-function setCategories(data) {
+var categories,
+    currentRound,
+    clueVals;
 
+function populateCategories(data) {
+    categories = {};
     for (var i = 0; i < data.length; i++) {
-        categoriesThisRound[data[i].category.title] = data[i].category.id;
+        var catName = data[i].category.title;
+
+        categories[catName] = {
+            id: data[i].category.id,
+            questions: []
+        }
+
+        // Populate Category divs
         $('.column:nth-child(' + (i + 1) + ') .category').text(data[i].category.title.toUpperCase());
     }
-
-    var categoryIDs = [];
-    for (var category of data) {
-        var clue = $.ajax({
-            method: 'GET',
-            dataType: 'json',
-            url: 'http://jservice.io/api/clues?category=' + category.category_id
-        });
-        categoryIDs.push(clue);
-    }
-    Promise.all(categoryIDs).then(setQuestionValues);
 }
 
-function setQuestionValues(categories) {
-
-    for (var category of categories) {
-        var questions = [];
-        for (var question of category) {
-            // console.log("Question: ", question.question);
-            questions.push(question);
-        }
-        questions.sort(function(a, b) {
-            return a.value - b.value;
+function getQuestions(data) {
+    var promises = [];
+    for (var cat of data) {
+        var prom = $.ajax({
+            method: 'GET',
+            dataType: 'json',
+            url: 'http://jservice.io/api/clues?category=' + cat.category_id
         });
-        // console.log(questions, questions.length);
+        promises.push(prom);
+    }
+    Promise.all(promises).then(function(data) {
+        for (var category of data) {
+            for (var i = 0; i < category.length; i++) {
+                var catName = category[i].category.title;
+                categories[catName].questions.push(category[i]);
+            }
+        }
+    });
+}
 
-        // for (var i = 1; i < 7; i++) {
-        //   var $column = $('.column:nth-of-type(' + i +')');
-        //   console.log("i:", i);
-        //   for (var j = 1; j < 6; j++) {
-        //     console.log("j:", j);
-        //   }
+function attachHandlers() {
+    $('.question').click(function(e) {
+        var clickedVal = Number($(this).text().replace('$', ''));
+        var category = $(this).parent().children('.category').text().toLowerCase();
+        var questions = categories[category].questions;
+        for (var question of questions) {
+            if (question.value === clickedVal) {
+                console.log(question.question);
+                break;
+            }
+        }
+    });
+}
 
-            // console.log(questions[i]);
-        // }
+function checkRound() {
+    if (currentRound === 1) {
+        clueVals = [200, 400, 600, 800, 1000];
+    } else {
+        clueVals = [400, 800, 1200, 1600, 2000];
     }
 }
